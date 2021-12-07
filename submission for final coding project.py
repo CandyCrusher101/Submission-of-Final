0192acdbcfe7fed967a7a944d621d44b52c6bd79
CandyCrusher101 <93202353+CandyCrusher101@users.noreply.github.com>
2021-12-06 21:24:14 -0500

    Add files via upload
    THIS THING HERE HAS EVERYTHING

---
+++ submission for final coding project.py
@@ -0,0 +1,24 @@
+#NISA ZAMAN
+#FINAL CODING PROJECT
+
+#This final project was uploaded to itch.io and is accessible for download from that website
+
+
+#IMPORTANT LINKS:
+
+#Youtube: https://youtu.be/_q1lkx4MxnE
+    #This is unlisted, so please use this to help find the video
+
+#Link to download the actual game:
+    #Mac: https://candycrusher101.itch.io/a-day-with-benny
+    #PC: https://candycrusher101.itch.io/a-day-with-benny-for-pc
+
+#Download the game from itch.io (username is CandyCrusher101)
+#This is a zip file, once you open it up, you can play the game
+
+#If a message comes up saying it won't be able to open up because its from the internet, go to system preferences and change it so that you can open applications downloaded from the internet
+#This is not a virus, your computer is just trying to keep you safe
+#For apple: system preferences>security and privacy>general>"allow apps downloaded from..." and allow the game to run.
+
+#I tested this out on my brother's computer and it worked
+#Ignore all the other repositories, that was just proof of me trying to figure out how to upload the game directly to github lol
